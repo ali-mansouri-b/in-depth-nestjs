dd7c37695cb5c8b99a53b712444cdd1a8fbc8637
ali-mansouri-b <ali.mansouribot@gmail.com>
2025-05-27 19:38:43 +0330

    feat: return serialized response

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Inject,
+  Param,
+  SerializeOptions,
+  UseInterceptors,
+} from '@nestjs/common';
+import { UsersService } from 'src/users/services/users/users.service';
+import { SerializedUser } from 'src/users/types/user';
 
+@UseInterceptors(ClassSerializerInterceptor)
+@SerializeOptions({ type: SerializedUser })
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(
+    @Inject('USER_SERVICE') private readonly usersService: UsersService,
+  ) {}
+
+  @Get()
+  getUsers() {
+    return this.usersService.getUsers();
+  }
+
+  @Get(':username')
+  getUserByUsername(@Param('username') username: string) {
+    const user = this.usersService.getUserByUsername(username);
+    if (user) return user;
+    else throw new HttpException('user not found', HttpStatus.NOT_FOUND);
+  }
+}
--- src/users/services/users/users.service.ts
+++ src/users/services/users/users.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/users/types/user';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = [
+    {
+      username: 'abcd',
+      password: 'abcd',
+    },
+    {
+      username: 'efgh',
+      password: 'efgh',
+    },
+    {
+      username: 'ijkl',
+      password: 'ijkl',
+    },
+    {
+      username: 'mnop',
+      password: 'mnop',
+    },
+  ];
+
+  getUsers() {
+    return this.users;
+  }
+
+  getUserByUsername(username: string) {
+    return this.users.find((item) => item.username === username);
+  }
+}
